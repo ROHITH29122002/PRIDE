c37f32be35c6ebc012fce08df3fc9bb53c2c12f8
Rohith Challa <rohithsince2002@gmail.com>
2022-03-05 18:11:04 +0530

    define Js for links.

--- javascript/index.js
+++ javascript/index.js
@@ -2,16 +2,16 @@ let mycourses = document.querySelector('.mycourses');
 let courses = document.querySelector('.courses');
 let myaboutus = document.querySelector('.myaboutus');
 let aboutus = document.querySelector('.about_us');
-let navbar = document.querySelector('.navbar');
+let navleft = document.querySelector('.navleft');
 mycourses.addEventListener("mouseenter",function (){
     courses.style.display='grid';
 })
-navbar.addEventListener("mouseleave",function (){
+navleft.addEventListener("mouseleave",function (){
     courses.style.display='none';
 })
 myaboutus.addEventListener("mouseenter",function (){
     aboutus.style.display='grid';
 })
-navbar.addEventListener("mouseleave",function (){
+navleft.addEventListener("mouseleave",function (){
     aboutus.style.display='none';
 })
